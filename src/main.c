4370afc33e1a7d9af8f579b5dac2a5eab3ee78ec
ghmaimon <kaixokoko123@gmail.com>
2021-01-30 16:01:32 +0100

    added glade widget and initial main.c code

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+
+GtkBuilder *builder;
+GtkWidget *window;
+GtkWidget *ajAdh;
+GtkWidget *ajLiv;
+GtkWidget *radAdh;
+GtkWidget *radLiv;
+
+int main(){
+
+    return 0;
+}
