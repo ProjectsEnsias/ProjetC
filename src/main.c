52b615f80322dd844d9a6607cdb9984b71b30e89
ghmaimon <kaixokoko123@gmail.com>
2021-02-02 15:20:02 +0100

    example of button population

--- src/main.c
+++ src/main.c
@@ -23,6 +23,8 @@ GtkWidget *catPort;
 GtkWidget *radGrid;
 GtkWidget *ra;
 GtkWidget *rl;
+GtkWidget *all;
+GtkWidget *spec;
 GtkWidget *cats;
 GtkWidget *catGrid;
 GtkWidget *list;
@@ -32,10 +34,11 @@ GtkWidget *adhs;
 GtkWidget *searchB;
 GtkWidget *searchEnt;
 
+
 // les variables et lists utilisés :
 
 list_adherent head;
-
+int numButtons;
 
 int main(int argc,char **argv){
     
@@ -46,6 +49,8 @@ int main(int argc,char **argv){
     mainGrid = GTK_WIDGET(gtk_builder_get_object(builder,"mainGrid"));
     header = GTK_WIDGET(gtk_builder_get_object(builder,"header"));
     panel = GTK_WIDGET(gtk_builder_get_object(builder,"panel"));
+    all = GTK_WIDGET(gtk_builder_get_object(builder,"all"));
+    spec = GTK_WIDGET(gtk_builder_get_object(builder,"spec"));
     addA = GTK_WIDGET(gtk_builder_get_object(builder,"addA"));
     addL = GTK_WIDGET(gtk_builder_get_object(builder,"addL"));
     sideNav = GTK_WIDGET(gtk_builder_get_object(builder,"sideNav"));
@@ -60,7 +65,25 @@ int main(int argc,char **argv){
     radGrid = GTK_WIDGET(gtk_builder_get_object(builder,"radGrid"));
     list = GTK_WIDGET(gtk_builder_get_object(builder,"list"));
     typeL = GTK_WIDGET(gtk_builder_get_object(builder,"typeL"));
-    searchEnt = GTK_WIDGET(gtk_builder_get_object(builder,"searchEnt"))
+    searchEnt = GTK_WIDGET(gtk_builder_get_object(builder,"searchEnt"));
+    int lign = 0;
+    int colon = 2;
+    for (int i = 0; i < numButtons; i++)
+    {
+        if(colon == 2){
+            colon = 0;
+            lign++;
+            gtk_grid_insert_row(GTK_GRID(adhs),2*lign);
+        }
+        colon++;
+        buttons[i] = gtk_button_new_with_label("\t\t\tehm\t\t\t");
+        gtk_widget_set_hexpand (buttons[i], TRUE);
+        gtk_widget_set_vexpand (buttons[i], TRUE);
+        gtk_widget_set_valign (buttons[i], GTK_ALIGN_CENTER);
+        gtk_widget_set_halign (buttons[i], GTK_ALIGN_CENTER);
+        gtk_grid_attach(GTK_GRID(adhs),buttons[i],i%3,i/3,1,1);
+    }
+    
 
 
     g_signal_connect(mainWindow,"destroy",G_CALLBACK(destr),NULL);
